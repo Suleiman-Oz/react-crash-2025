bc6360ac81bb9bde4e3869f41c7fd38007d1ae8b
Suleiman <sulemancusman@gmail.com>
2025-07-15 13:39:03 +0300

    lesson 01 - 03 finished

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello React Crash Course</h1>
+        <Youtube />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from './Youtube.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Vite + React Crash Course</h1>
+      <Youtube />
+    </>
+  )
+}
+
+export default App
